09ed05ed02ae67f6cdf0ffa898cfe29074069e32
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-13 09:25:49 -0700

    initial commit

---
+++ Conditionals.java
@@ -0,0 +1,24 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        String option = "";
+
+        int random = (int) (Math.random() * 3 + 1);
+        
+        switch (random) {
+            case 1: option = "hit"; 
+            break;
+            case 2: option = "stay";
+            break;
+            case 3: option = "fold";
+            break;
+            default: option = "This should never get called";
+        }
+     
+
+        if (!option.equals("hit") && !option.equals("stay")) {
+            System.out.println("fold");
+        } else {
+            System.out.println(option); 
+       } 
+    }
+}
